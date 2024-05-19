65bc91675d06611a0b7c7b331cc313832f1d39f3
Evandro Santos <vandersant7@gmail.com>
2024-05-19 20:06:08 -0300

    feat: create home page, quizz component; style: apply global styles and style quizz component

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
+import quizz_questions from '../../../assets/data/quizz_questions.json'
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf, NgFor
+  ],
   templateUrl: './quizz.component.html',
-  styleUrl: './quizz.component.css'
+  styleUrls: ['./quizz.component.css'],
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+  title: string = '';
 
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answerSelected: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+    }
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    console.log(this.answers)
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { QuizzComponent } from "../../components/quizz/quizz.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    imports: [QuizzComponent]
 })
 export class HomeComponent {
 

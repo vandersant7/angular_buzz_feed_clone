0f8a43f744ecde7cceab70b5945f4165764dc375
Evandro Santos <vandersant7@gmail.com>
2024-05-19 23:45:55 -0300

    feat: add function nextStep and checkResult

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
-import quizz_questions from '../../../assets/data/quizz_questions.json'
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [
-    NgIf, NgFor
-  ],
+  imports: [NgIf, NgFor],
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css'],
 })
@@ -28,21 +26,46 @@ export class QuizzComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    if(quizz_questions){
-      this.finished = false
-      this.title = quizz_questions.title
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
 
-      this.questions = quizz_questions.questions
-      this.questionSelected = this.questions[this.questionIndex]
-      this.questionMaxIndex = this.questions.length
-
-      console.log(this.questionIndex)
-      console.log(this.questionMaxIndex)
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+      this.questionMaxIndex = this.questions.length;
     }
   }
 
-  playerChoose(value:string){
-    this.answers.push(value)
-    console.log(this.answers)
+  playerChoose(value: string) {
+    this.answers.push(value);
+    this.nextStep();
+    console.log(this.answers);
   }
+
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers)
+      this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
+    }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === previous).length
+      ) {
+        return previous
+      } else {
+        return current
+      }
+    });
+    return result
+  }
+
 }
